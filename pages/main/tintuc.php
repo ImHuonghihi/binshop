c3f9434b703b8c1f93b3ffa3ed4b5356418ed871
Phạm Thị Lan Hương <88477341+phamthilanhuong@users.noreply.github.com>
2024-02-17 16:14:15 +0700

    ok

---
+++ pages/main/tintuc.php
@@ -0,0 +1,10 @@
+<p>Sales</p>
+<ul>
+	<li> <a href="https://www.thegioididong.com/dtdd/iphone-12-pro-max">iphone12</a></li>
+	<li> <a href="https://www.thegioididong.com/dtdd/realme-8">Realme8</a></li>
+	<li> <a href="https://www.thegioididong.com/dtdd/vivo-v21-5g">Vivo-V21</a></li>
+	<li> <a href="https://www.thegioididong.com/dtdd/xiaomi-redmi-note-10">Redmi-note-10</a></li>
+	<li> <a href="https://www.thegioididong.com/dtdd/iphone-11">iphone-11</a></li>
+	
+	
+</ul>
